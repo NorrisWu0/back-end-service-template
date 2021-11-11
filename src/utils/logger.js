1172ce7ecae39b403035bbca3ffa3974d3f78553
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-11-11 16:50:41 +1100

    Updated logger output format

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,9 +4,9 @@ const logger = winston.createLogger({
   level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   format: winston.format.combine(
     winston.format.colorize(),
-    winston.format.timestamp({ format: 'HH:mm:ss' }),
+    winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
     winston.format.printf(
-      (info) => `${info.timestamp} ${info.level} ${info.message}`
+      (info) => `[${info.timestamp} - ${info.level}] ${info.message}`
     )
   ),
   transports: [new winston.transports.Console()]
