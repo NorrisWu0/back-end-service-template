0b48a51d08e4eb13247634542de9d7a4c475d614
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-11-11 16:32:16 +1100

    Installed and setup `morgan, helmet`

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
 const router = require('./routes');
 
 const app = express();
 app.use(cors());
+app.use(
+  process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev')
+);
+app.use(helmet());
 
 app.use('/api/v1', router);
 
