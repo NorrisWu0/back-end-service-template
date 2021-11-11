061fdf35159d82412b16cdd637cb54d7d61d8bb5
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-11-11 15:40:38 +1100

    Initialized express server to listen on `.env.PORT`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+const app = require('./app');
+const logger = require('./utils/logger');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  logger.info(`listening on port ${PORT}`);
+});
